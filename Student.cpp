55cfc0ef2393968a7b97f27150ddbc84ad400835
nyurlov <mrmamongo@gmail.com>
2022-05-08 19:42:07 +0300

    added readme

---
+++ Student.cpp
@@ -0,0 +1,5 @@
+//
+// Created by MrMam on 08.05.2022.
+//
+
+#include "Student.h"
---
+++ Student.h
@@ -0,0 +1,14 @@
+//
+// Created by MrMam on 08.05.2022.
+//
+
+#ifndef STUDENTSTORAGE_STUDENT_H
+#define STUDENTSTORAGE_STUDENT_H
+
+
+class Student {
+
+};
+
+
+#endif //STUDENTSTORAGE_STUDENT_H
